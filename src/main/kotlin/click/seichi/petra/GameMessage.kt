12a1d842df364f4625fc39b72fff76a267422756
tar0ss <syas1215@icloud.com>
2020-05-10 14:14:57 +0900

    Collect message

---
+++ src/main/kotlin/click/seichi/petra/GameMessage.kt
@@ -0,0 +1,32 @@
+package click.seichi.petra
+
+import click.seichi.message.Message
+import click.seichi.message.TitleMessage
+import click.seichi.util.Random
+import org.bukkit.ChatColor
+
+/**
+ * @author tar0ss
+ */
+object GameMessage {
+    val READY_GAME: (Int, Int) -> Message = { ready: Int, all: Int ->
+        TitleMessage(
+                "${ChatColor.WHITE}ゲームスタート $ready / $all",
+                "${ChatColor.BLUE}/r で じゅんび",
+                stay = Int.MAX_VALUE
+        )
+    }
+    val START_COUNT: (Int) -> Message = { remainSeconds: Int ->
+        TitleMessage(
+                "${Random.nextChatColor()}$remainSeconds",
+                "${ChatColor.RED}/r で キャンセル",
+                fadeIn = 5,
+                stay = 10,
+                fadeOut = 10
+        )
+    }
+    val START_GAME: Message = TitleMessage(
+            "${ChatColor.YELLOW}スタート",
+            ""
+    )
+}
--- src/main/kotlin/click/seichi/petra/listener/GameListener.kt
+++ src/main/kotlin/click/seichi/petra/listener/GameListener.kt
@@ -4,8 +4,7 @@ import click.seichi.game.event.GameStartCountEvent
 import click.seichi.game.event.PlayerCancelReadyEvent
 import click.seichi.game.event.PlayerReadyEvent
 import click.seichi.game.event.StartGameEvent
-import click.seichi.message.TitleMessage
-import click.seichi.util.Random
+import click.seichi.petra.GameMessage
 import org.bukkit.ChatColor
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
@@ -20,41 +19,23 @@ class GameListener(private val players: Set<Player>) : Listener {
     fun onPlayerReady(event: PlayerReadyEvent) {
         val player = event.player
         player.setPlayerListName("${ChatColor.GREEN}${player.name}")
-        updateMessage(event.ready, event.all)
+        GameMessage.READY_GAME(event.ready, event.all).broadcastTo { players.contains(it) }
     }
 
     @EventHandler
     fun onPlayerCancelReady(event: PlayerCancelReadyEvent) {
         val player = event.player
         player.setPlayerListName("${ChatColor.WHITE}${player.name}")
-        updateMessage(event.ready, event.all)
-    }
-
-    private fun updateMessage(ready: Int, all: Int) {
-        TitleMessage(
-                "${ChatColor.WHITE}ゲームスタート $ready / $all",
-                "${ChatColor.BLUE}/r で じゅんび",
-                stay = Int.MAX_VALUE
-        ).broadcastTo { players.contains(it) }
+        GameMessage.READY_GAME(event.ready, event.all).broadcastTo { players.contains(it) }
     }
 
     @EventHandler
     fun onGameStartCount(event: GameStartCountEvent) {
-        val remainSeconds = event.remainSeconds
-        TitleMessage(
-                "${Random.nextChatColor()}$remainSeconds",
-                "${ChatColor.RED}/r で キャンセル",
-                fadeIn = 5,
-                stay = 10,
-                fadeOut = 10
-        ).broadcastTo { players.contains(it) }
+        GameMessage.START_COUNT(event.remainSeconds).broadcastTo { players.contains(it) }
     }
 
     @EventHandler
     fun onStartGame(event: StartGameEvent) {
-        TitleMessage(
-                "${ChatColor.YELLOW}スタート",
-                ""
-        ).broadcastTo { players.contains(it) }
+        GameMessage.START_GAME.broadcastTo { players.contains(it) }
     }
 }
