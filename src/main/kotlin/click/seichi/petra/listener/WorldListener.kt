16357338748942f9d35ab57f7545ab3d218cb928
tar0ss <syas1215@icloud.com>
2020-05-12 20:52:32 +0900

    Add world listener

---
+++ src/main/kotlin/click/seichi/petra/listener/WorldListener.kt
@@ -0,0 +1,35 @@
+package click.seichi.petra.listener
+
+import org.bukkit.Difficulty
+import org.bukkit.GameRule
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.world.WorldInitEvent
+
+/**
+ * @author tar0ss
+ */
+class WorldListener : Listener {
+    @EventHandler
+    fun onWorldInit(event: WorldInitEvent) {
+        val world = event.world
+        world.setSpawnFlags(false, false)
+        world.pvp = false
+        world.difficulty = Difficulty.NORMAL
+        world.isAutoSave = true
+        world.setGameRule(GameRule.ANNOUNCE_ADVANCEMENTS, false)
+        world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, true)
+        world.setGameRule(GameRule.DO_WEATHER_CYCLE, false)
+        world.setGameRule(GameRule.SPECTATORS_GENERATE_CHUNKS, false)
+        world.setGameRule(GameRule.DO_MOB_SPAWNING, false)
+        world.setGameRule(GameRule.DISABLE_ELYTRA_MOVEMENT_CHECK, true)
+        world.setGameRule(GameRule.DO_FIRE_TICK, false)
+        world.setGameRule(GameRule.DO_LIMITED_CRAFTING, false)
+        world.setGameRule(GameRule.SPAWN_RADIUS, 0)
+        world.worldBorder.setCenter(0.0, 0.0)
+        world.worldBorder.size = 1000.0
+        world.worldBorder.warningDistance = 0
+        world.worldBorder.warningTime = 0
+    }
+
+}
