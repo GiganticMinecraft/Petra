39d4b57e0a624ee1100c302979643f234986edd3
tar0ss <syas1215@icloud.com>
2020-05-22 23:07:45 +0900

    Change enum name

--- src/main/kotlin/click/seichi/petra/stage/StageResult.kt
+++ src/main/kotlin/click/seichi/petra/stage/StageResult.kt
@@ -10,6 +10,6 @@ import click.seichi.petra.GameSound
 enum class StageResult(val message: Message) {
     WIN(TitleMessage("勝利!!", "").add(GameSound.WIN)),
     DEATH_ALL_PLAYERS(TitleMessage("敗北", "全滅してしまった").add(GameSound.LOSE)),
-    DEATH_TARGET_PLAYERS(TitleMessage("敗北", "守り切れなかった").add(GameSound.LOSE)),
+    DEATH_TARGET_ENTITY(TitleMessage("敗北", "守り切れなかった").add(GameSound.LOSE)),
     OVER_THE_TIME_LIMIT(TitleMessage("敗北", "時間切れ").add(GameSound.LOSE))
 }
