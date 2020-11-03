e5779f8ffcc3bdf7dddba76ec63172e6b2d73934
tar0ss <syas1215@icloud.com>
2020-11-03 12:48:07 +0900

    Change start time

--- src/main/kotlin/click/seichi/petra/stage/Stage.kt
+++ src/main/kotlin/click/seichi/petra/stage/Stage.kt
@@ -29,7 +29,7 @@ enum class Stage(
             SmallHalloween.KEY,
             SmallHalloween.GENERATOR,
             SmallHalloween.SPAWN_PROXY,
-            6000L,
+            0L,
             4..4,
             *SmallHalloween.SECTIONS
     )
