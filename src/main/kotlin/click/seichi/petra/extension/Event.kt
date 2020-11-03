fb5690995d8f3afdd643ddef6fa06bd2bf1d3a8b
tar0ss <syas1215@icloud.com>
2020-11-03 19:58:12 +0900

    fix

--- src/main/kotlin/click/seichi/petra/extension/Event.kt
+++ src/main/kotlin/click/seichi/petra/extension/Event.kt
@@ -1,11 +1,13 @@
 package click.seichi.petra.extension
 
 import click.seichi.petra.Plugin
+import click.seichi.petra.function.debug
 import org.bukkit.event.Listener
 
 /**
  * @author tar0ss
  */
 fun Listener.register() = Plugin.INSTANCE.let {
+    debug("register listener ${this::class.simpleName}")
     it.server.pluginManager.registerEvents(this, it)
 }
