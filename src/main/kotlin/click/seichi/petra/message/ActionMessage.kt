be85ff1df848ccdfde19c84cf088b1d065e224d0
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-01-18 12:59:48 +0900

    fix: player#sendActionBarをSpigotを利用した形に

--- src/main/kotlin/click/seichi/petra/message/ActionMessage.kt
+++ src/main/kotlin/click/seichi/petra/message/ActionMessage.kt
@@ -1,5 +1,7 @@
 package click.seichi.petra.message
 
+import net.md_5.bungee.api.ChatMessageType
+import net.md_5.bungee.api.chat.TextComponent
 import org.bukkit.entity.Player
 
 /**
@@ -8,7 +10,7 @@ import org.bukkit.entity.Player
 class ActionMessage(private val message: String) : Message() {
 
     override fun sendToSub(player: Player) {
-        player.sendActionBar(message)
+        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, *TextComponent.fromLegacyText(message))
     }
 
 }
